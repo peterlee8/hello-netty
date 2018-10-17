ececa513d2d5359c9901fe3db25a084b94f46c27
peterlee8 <15927341500@163.com>
2018-10-17 23:36:48 +0800

    add fastDFS

--- imooc-muxin-netty/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ imooc-muxin-netty/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,9 +1,170 @@
 package com.imooc.service.impl;
 
+import com.imooc.mapper.*;
+import com.imooc.pojo.ChatMsg;
+import com.imooc.pojo.Users;
+import com.imooc.pojo.vo.FriendRequestVO;
+import com.imooc.pojo.vo.MyFriendsVO;
+import com.imooc.service.UserService;
+import com.imooc.utils.FastDFSClient;
+import com.imooc.utils.FileUtils;
+import com.imooc.utils.QRCodeUtils;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.entity.Example.Criteria;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @Author peterLee
  * @Date 2018/10/13 上午10:01
  * @Describtion :
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UsersMapper userMapper;
+
+    @Autowired
+    private UsersMapperCustom usersMapperCustom;
+
+    @Autowired
+    private MyFriendsMapper myFriendsMapper;
+
+    @Autowired
+    private FriendsRequestMapper friendsRequestMapper;
+
+    @Autowired
+    private ChatMsgMapper chatMsgMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Autowired
+    private QRCodeUtils qrCodeUtils;
+
+   /* @Autowired
+    private FastDFSClient fastDFSClient;*/
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+
+        Users user = new Users();
+        user.setUsername(username);
+
+        Users result = userMapper.selectOne(user);
+
+        return result != null ;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserForLogin(String username, String pwd) {
+
+        Example userExample = new Example(Users.class);
+        Criteria criteria = userExample.createCriteria();
+
+        criteria.andEqualTo("username", username);
+        criteria.andEqualTo("password", pwd);
+
+        Users result = userMapper.selectOneByExample(userExample);
+
+        return result;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users saveUser(Users user) {
+
+        String userId = sid.nextShort();
+
+        /*// 为每个用户生成一个唯一的二维码
+        String qrCodePath = "/Users/ligang/IdeaProjects/netty/netty-hello/imooc-muxin-netty/src/main/resources/qrCode" + userId + "qrcode.png";
+        // muxin_qrcode:[username]
+        qrCodeUtils.createQRCode(qrCodePath, "muxin_qrcode:" + user.getUsername());
+        MultipartFile qrCodeFile = FileUtils.fileToMultipart(qrCodePath);
+
+        String qrCodeUrl = "";
+        try {
+            qrCodeUrl = fastDFSClient.uploadQRCode(qrCodeFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user.setQrcode(qrCodeUrl);*/
+
+        user.setId(userId);
+        userMapper.insert(user);
+
+        return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(Users user) {
+        userMapper.updateByPrimaryKeySelective(user);
+        return queryUserById(user.getId());
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    Users queryUserById(String userId) {
+        return userMapper.selectByPrimaryKey(userId);
+    }
+
+    @Override
+    public Integer preconditionSearchFriends(String myUserId, String friendUsername) {
+        return null;
+    }
+
+    @Override
+    public Users queryUserInfoByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void sendFriendRequest(String myUserId, String friendUsername) {
+
+    }
+
+    @Override
+    public List<FriendRequestVO> queryFriendRequestList(String acceptUserId) {
+        return null;
+    }
+
+    @Override
+    public void deleteFriendRequest(String sendUserId, String acceptUserId) {
+
+    }
+
+    @Override
+    public void passFriendRequest(String sendUserId, String acceptUserId) {
+
+    }
+
+    @Override
+    public List<MyFriendsVO> queryMyFriends(String userId) {
+        return null;
+    }
+
+    @Override
+    public String saveMsg(ChatMsg chatMsg) {
+        return null;
+    }
+
+    @Override
+    public void updateMsgSigned(List<String> msgIdList) {
+
+    }
+
+    @Override
+    public List<ChatMsg> getUnReadMsgList(String acceptUserId) {
+        return null;
+    }
 }
